e1db8ebe7f24505f1a30698e2ae3c334705e97c5
Tommy Vange Rød <28400191+tommyvange@users.noreply.github.com>
2024-06-06 10:28:09 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,9 +4,9 @@
 # Developer: Tommy Vange Rød
 # License: GPL 3.0 License
 #
-# This file is part of Printer Management Scripts.
+# This file is part of "Printer Management Scripts".
 #
-# Printer Management Scripts is free software: you can redistribute it and/or modify
+# "Printer Management Scripts" is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
